6159ba754be85eb3aee5b30520099c43ae38cff0
Karen Smetana <karen.smetana@turner.com>
2014-04-05 19:34:08 -0700

    Add twitter API KEY and sceret

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'action_controller/railtie'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
+require 'omniauth'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-	
+Rails.application.config.middleware.use OmniAuth::Builder do	
     provider :twitter, ENV['eZHpmjVCOhmmwQSvGFopbOpwT'], ENV['2vqPUvvvGxWfQnjfTiBGDWMG2FNXHdexfXufMwFptvpOupF8rl']
 end
