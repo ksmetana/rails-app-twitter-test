9995659db80645e3f851b17259d4f896b8cf64a5
Karen Smetana <karen.smetana@turner.com>
2014-04-05 18:45:06 -0700

    Init Repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ private
 
   def client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.oauth_token = session['access_token']
-      config.oauth_token_secret = session['access_token_secret']
+      config.consumer_key = ENV['eZHpmjVCOhmmwQSvGFopbOpwT']
+      config.consumer_secret = ENV['2vqPUvvvGxWfQnjfTiBGDWMG2FNXHdexfXufMwFptvpOupF8rl']
+      config.oauth_token = session['10914232-U3p9i9XJ0c4szCjkuFs6YVQqU1K7X6VkKt8fcbtvK']
+      config.oauth_token_secret = session['CSQZkGQ1EmmJGFrmGv4cAIv9JDxGqSbJgP9WZ0qMXRAVI']
     end
   end
 end
