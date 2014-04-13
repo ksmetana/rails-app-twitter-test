61ff0a942fa77700681722b494dee43a5a52ec36
Karen Smetana <karen.smetana@turner.com>
2014-04-12 18:29:20 -0700

    show first 20 friends

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def show
     if session['access_token'] && session['access_token_secret']
       @user = client.user(include_entities: true)
-      @friends = client.friends("ksmetana")
+      @friends = client.friends.take(10)
     else
       redirect_to failure_path
     end
