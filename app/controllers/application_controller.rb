32f8c83fc2580f3f81e2d0d3727b309f930cf0fc
Karen Smetana <karen.smetana@turner.com>
2014-04-30 17:28:04 -0700

    add d3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,15 @@ private
       config.consumer_secret = "2vqPUvvvGxWfQnjfTiBGDWMG2FNXHdexfXufMwFptvpOupF8rl"
       config.oauth_token = "10914232-l8K00T673ML3EjEGPpXorOOVIx5pMWa3aW4KKNbUK"
       config.oauth_token_secret = "o198WS4Hs9LVigz3zlfuSnIJlm1oOUCdzRs1vjdkeOps0"
+
+##Tumblr.configure do |config|
+  @client ||= Tumblr::REST::Client.new do |config|
+  config.consumer_key = "consumer_key"
+  config.consumer_secret = "consumer_secret"
+  config.oauth_token = "access_token"
+  config.oauth_token_secret = "access_token_secret"
+end
+
     end
   end
 end
