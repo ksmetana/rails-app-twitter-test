1cae48c3ee491128e935bdfd501b34e849fff0a3
Karen Smetana <karen.smetana@turner.com>
2014-04-30 18:04:32 -0700

    remove tm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 private
-
   def client
-
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key = "eZHpmjVCOhmmwQSvGFopbOpwT"
       config.consumer_secret = "2vqPUvvvGxWfQnjfTiBGDWMG2FNXHdexfXufMwFptvpOupF8rl"
       config.oauth_token = "10914232-l8K00T673ML3EjEGPpXorOOVIx5pMWa3aW4KKNbUK"
       config.oauth_token_secret = "o198WS4Hs9LVigz3zlfuSnIJlm1oOUCdzRs1vjdkeOps0"
-
-##Tumblr.configure do |config|
-  @client ||= Tumblr::REST::Client.new do |config|
-  config.consumer_key = "consumer_key"
-  config.consumer_secret = "consumer_secret"
-  config.oauth_token = "access_token"
-  config.oauth_token_secret = "access_token_secret"
-end
-
     end
-  end
+  end 
+
 end
