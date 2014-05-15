0b69fef65a7e374725583e8cb64a0fe6bdd763be
Karen Smetana <karen.smetana@turner.com>
2014-05-14 18:55:02 -0700

    change d3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
   def show
     if session['access_token'] && session['access_token_secret']
       @user = client.user(include_entities: true)
-      @friends = client.friends.take(20)
+      # @friends = client.friends.take(20)
+      @friends = client.friends.to_a
 
       @user1 = client.user("hlntv")
       @friends1 = client.friends.take(20)
