bb43b4679e7bcfbe83db195a1b7cb06c4a64c2a5
Karen Smetana <karen.smetana@turner.com>
2014-06-09 18:25:52 -0700

    extend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,51 @@ class SessionsController < ApplicationController
     redirect_to show_path, notice: 'Signed in'
   end
 
+  def fetch_all_friends(twitter_username, max_attempts = 100)
+    # in theory, one failed attempt will occur every 15 minutes, so this could be long-running
+    # with a long list of friends
+    num_attempts = 0
+    #client = twitter_client
+    myfile = File.new("#{twitter_username}_friends_list.txt", "w")
+    running_count = 0
+    cursor = -1
+    list = []
+    while (cursor != 0) do
+      begin
+        num_attempts += 1
+        # 200 is max, see https://dev.twitter.com/docs/api/1.1/get/friends/list
+        friends = client.friends(twitter_username, {:cursor => cursor, :count => 200} )
+        friends.each do |f|
+          running_count += 1
+          list << f
+          myfile.puts "\"#{running_count}\",\"#{f.name.gsub('"','\"')}\",\"#{f.screen_name}\",\"#{f.url}\",\"#{f.followers_count}\",\"#{f.location.gsub('"','\"').gsub(/[\n\r]/," ")}\",\"#{f.created_at}\",\"#{f.description.gsub('"','\"').gsub(/[\n\r]/," ")}\",\"#{f.lang}\",\"#{f.time_zone}\",\"#{f.verified}\",\"#{f.profile_image_url}\",\"#{f.website}\",\"#{f.statuses_count}\",\"#{f.profile_background_image_url}\",\"#{f.profile_banner_url}\""
+        end
+        puts "#{running_count} done"
+        cursor = friends.next_cursor
+        break if cursor == 0
+      rescue Twitter::Error::TooManyRequests => error
+        if num_attempts <= max_attempts
+          cursor = friends.next_cursor if friends && friends.next_cursor
+          puts "#{running_count} done from rescue block..."
+          puts "Hit rate limit, sleeping for #{error.rate_limit.reset_in}..."
+          sleep error.rate_limit.reset_in
+          retry
+        else
+          raise
+        end
+      end
+    end
+    return list
+  end
+
+
   def show
     if session['access_token'] && session['access_token_secret']
       @user = client.user(include_entities: true)
-      @friends = client.friends.take(15)
-
-      @user1 = client.user("hlntv")
-      @friends1 = client.friends.take(15)
+      #@friends = client.friends.take(15)
+      @friends = fetch_all_friends('ksmetana', 100)
+      #@user1 = client.user("hlntv")
+      #@friends1 = client.friends.take(15)
 
       gon.friends =  @friends
     else
